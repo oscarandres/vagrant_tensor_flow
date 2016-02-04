5490b6e36b5d5cc43a7ae76672a04f4102ccceda
Oscar Andrés Monroy <oscar.andres.monroy@gmail.com>
2016-02-04 17:08:55 -0300

    Uncommenting apt-get update

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
-#sudo apt-get update
+sudo apt-get update
 sudo apt-get install -y python-pip python-dev
 sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.5.0-cp27-none-linux_x86_64.whl
